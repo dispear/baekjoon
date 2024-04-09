e085557b71b02ae1f6d7163989cd6c4ff6bda57e
dispear <87793524+dispear@users.noreply.github.com>
2024-04-09 14:18:57 +0900

    [Gold V] Title: 집합의 표현, Time: 1336 ms, Memory: 56172 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,40 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-public class Main {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int m = s.nextInt();
 
-		int[] arr = new int[n + 1];
-		for (int i = 0; i < n + 1; i++) {
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n+1];
+		for(int i=0;i<=n;i++) {
 			arr[i] = i;
 		}
-
-		for (int i = 0; i < m; i++) {
-			int a = s.nextInt();
-			int b = s.nextInt();
-			int c = s.nextInt();
-
-			if (a == 0) {
-				union(arr, b, c);
-			} else if (a == 1) {
-				if (find(arr, b) == find(arr, c)) {
+		
+		for(int i=0;i<m;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			if(a == 0) {
+				union(arr,b,c);
+			}else {
+				if(find(arr,b) == find(arr,c)) {
 					System.out.println("YES");
-				} else
+				}else
 					System.out.println("NO");
 			}
 		}
 	}
-
-	static void union(int[] arr, int n, int m) {
-		arr[find(arr, n)] = arr[find(arr, m)];
+	
+	static void union(int[] arr,int n1,int n2) {
+		arr[find(arr,n1)] = arr[find(arr,n2)];
 	}
-
-	static int find(int[] arr, int n) {
-		if (n == arr[n])
+	static int find(int[] arr,int n) {
+		if(n == arr[n])
 			return n;
 		else {
-			int a = find(arr, arr[n]);
+			int a = find(arr,arr[n]);
 			arr[n] = a;
 			return a;
 		}
