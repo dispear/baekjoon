f949968c5e5a7cafe5295a0d45cac3ca856d6e6b
dispear <87793524+dispear@users.noreply.github.com>
2024-04-10 15:02:48 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 220 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import javax.xml.validation.SchemaFactoryConfigurationError;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n][m];
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<m;j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<m;j++) {
+				arr[i][j] += Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				sb.append(arr[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
