e8d9389393e519d71a0216a5157a0dfdd5b4492f
dispear <87793524+dispear@users.noreply.github.com>
2024-04-10 17:19:07 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import javax.xml.validation.SchemaFactoryConfigurationError;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());	
+		int day = 0;
+		
+		day = (c - b -1) / (a - b) + 1;
+		
+		sb.append(day);
+		System.out.println(sb);
+	}
+}
