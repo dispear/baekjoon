0dd7a470e9a7a32feeed71ee1b03034f28ea6403
dispear <87793524+dispear@users.noreply.github.com>
2024-06-17 15:08:13 +0900

    [Gold IV] Title: 최단경로, Time: 780 ms, Memory: 108632 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,67 +1,74 @@
+import java.io.*;
 import java.util.*;
-
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
+		
+		st = new StringTokenizer(br.readLine());
+		int v = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		
+		ArrayList<NODE>[] arr = new ArrayList[v+1];
+		for(int i=1;i<=v;i++) {
+			arr[i] = new ArrayList<>();
+		}
+		for(int i=0;i<e;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			arr[a].add(new NODE(b, c));
+		}
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int v = s.nextInt();
-		int e = s.nextInt();
-		int start = s.nextInt();
-		boolean[] visited = new boolean[v + 1];
-		int[] dist = new int[v + 1];
-
-		for (int i = 1; i <= v; i++) {
-			if (i == start)
+		boolean[] visited = new boolean[v+1];
+		int[] dist = new int[v+1];
+		for(int i=1;i<=v;i++) {
+			if(i == start)
 				dist[i] = 0;
 			else
 				dist[i] = Integer.MAX_VALUE;
 		}
-
-		ArrayList<Node>[] arr = new ArrayList[v + 1];
-		for (int i = 1; i <= v; i++) {
-			arr[i] = new ArrayList<>();
-		}
-		for(int i=0;i<e;i++) {
-			int a = s.nextInt();
-			int b = s.nextInt();
-			int value = s.nextInt();
-			
-			arr[a].add(new Node(b,value));
-		}
 		
-		// 다익스트라
-		PriorityQueue<Node> que = new PriorityQueue<>((o1, o2) -> o1.value - o2.value);
-		que.offer(new Node(start,0));
-		visited[start] = true;
-		
+		PriorityQueue<NODE> que = new PriorityQueue<>((o1,o2)-> o1.w-o2.w);
+		que.offer(new NODE(start, 0));
+		dist[start] = 0;
 		while(!que.isEmpty()) {
-			Node n = que.poll();
-			visited[n.next] = true;
-			// n노드의 인접리스트 만큼 리스트
-			for(Node x : arr[n.next]) {
-				if(dist[x.next] > dist[n.next] + x.value) {
-					dist[x.next] = dist[n.next] + x.value;
-					que.offer(new Node(x.next, dist[x.next]));
+			NODE n = que.poll();
+			
+			if(dist[n.v]< n.w)
+				continue;
+			
+			for(int i=0;i<arr[n.v].size();i++) {
+				NODE newNode = arr[n.v].get(i);
+				
+				if(dist[newNode.v] > n.w + newNode.w) {
+					dist[newNode.v] = n.w + newNode.w;
+					que.offer(new NODE(newNode.v,dist[newNode.v]));
 				}
 			}
 		}
 		
 		for(int i=1;i<=v;i++) {
 			if(dist[i] == Integer.MAX_VALUE)
-				System.out.println("INF");
+				sb.append("INF\n");
 			else
-				System.out.println(dist[i]);
+				sb.append(dist[i]+"\n");
 		}
+		
+		System.out.println(sb);
 	}
-
 }
-
-class Node {
-	public int next;
-	public int value;
-
-	Node(int next, int value) {
-		this.next = next;
-		this.value = value;
+class NODE {
+	int v;
+	int w;
+	public NODE(int v,int w) {
+		this.v = v;
+		this.w = w;
 	}
 }
