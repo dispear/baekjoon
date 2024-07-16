bbc4ddcb61d7347ffb1d13c05ddb73aca1858420
dispear <87793524+dispear@users.noreply.github.com>
2024-07-16 19:20:30 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int sum = 0;
+		
+		for(int i=1;i<=Math.sqrt(n);i++) {
+			if(i*i <= n)
+				sum++;
+		}
+		
+		sb.append(sum);
+		System.out.println(sb);
+	}
+}
