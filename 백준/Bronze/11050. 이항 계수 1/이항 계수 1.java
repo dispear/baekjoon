492dfd0ea5bca4ec2586edd36fe8ddd7f81613a7
dispear <87793524+dispear@users.noreply.github.com>
2024-07-17 19:02:43 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,32 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr;
+	
 	public static void main(String[] args) throws IOException {
-		StringTokenizer st;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
+		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
+		
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		arr = new int[n+1][n+1];
 		
-		sb.append(c(n,m));
+		int result = con(n,m);
+		
+		sb.append(result);
 		System.out.println(sb);
 	}
-	
-	static int c(int n1,int n2) {
-		if(n1 == n2 || n2 == 0) {
-			return arr[n1][n2] = 1;
+	static int con(int n, int m) {
+		if(n == m || m == 0) {
+			return 1;
 		}
-		else if(n2 == 1) {
-			return arr[n1][n2] = n1;
-		}else {
-			return arr[n1][n2] = c(n1-1,n2) + c(n1-1,n2-1);
+		else if(m == 1) {
+			return n;
+		}
+		else {
+			return con(n-1,m-1)+con(n-1,m);
 		}
 	}
 }
