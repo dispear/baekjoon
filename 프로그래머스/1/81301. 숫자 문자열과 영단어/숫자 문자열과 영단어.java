8fc92e77e90ffd01ac747bcef8e3643442345bc4
dispear <87793524+dispear@users.noreply.github.com>
2024-08-04 16:57:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.49 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -3,26 +3,11 @@ class Solution {
         String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         int answer = 0;
         
-        String numstr = "";
-        String nextstr = "";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) >= '0' && s.charAt(i) <= '9' ){
-                numstr += s.charAt(i);
-            }else{
-                nextstr += s.charAt(i); 
-                
-                for(int j=0;j< 10;j++){
-                    if(nextstr.contains(str[j])){
-                         numstr += Integer.toString(j);
-                         
-                         nextstr = "";
-                         break;
-                    }
-                }
-            }
+        for(int i=0;i<10;i++){
+            s = s.replaceAll(str[i], Integer.toString(i));
         }
         
-        answer = Integer.parseInt(numstr);
+        answer = Integer.parseInt(s);
         return answer;
     }
 }
